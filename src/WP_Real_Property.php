7671e5fa99d2b43a3368c484dabfe86cc2d7c944
Shibu Vs <shibuvs89@gmail.com>
2024-12-12 09:18:43 +0530

    refactor class code

--- src/WP_Real_Property.php
+++ src/WP_Real_Property.php
@@ -3,15 +3,26 @@ namespace WPRealProperty;
 
 class WP_Real_Property {
 
+    /**
+     * Singleton instance
+     *
+     * @var WP_Real_Property|null
+     */
     private static $instance = null;
 
+    /**
+     * Private constructor to prevent direct instantiation
+     */
     private function __construct() {
-
-        add_action('init', array($this,'register_real_estate_post_type'));
-
-
+        // Hook to initialize the custom post type
+        add_action('init', [ $this, 'register_real_estate_post_type' ]);
     }
 
+    /**
+     * Get the singleton instance
+     *
+     * @return WP_Real_Property
+     */
     public static function getInstance() {
         if (self::$instance === null) {
             self::$instance = new self();
@@ -19,46 +30,45 @@ class WP_Real_Property {
         return self::$instance;
     }
 
+    /**
+     * Registers the Real Estate custom post type
+     */
     public function register_real_estate_post_type() {
-
-         $labels = [
-            'name'               => _x('Properties', 'post type general name', 'wp-real-property'),
-            'singular_name'      => _x('Property', 'post type singular name', 'wp-real-property'),
-            'menu_name'          => _x('Properties', 'admin menu', 'wp-real-property'),
-            'name_admin_bar'     => _x('Property', 'add new on admin bar', 'wp-real-property'),
-            'add_new'            => _x('Add New', 'property', 'wp-real-property'),
-            'add_new_item'       => __('Add New Property', 'wp-real-property'),
-            'new_item'           => __('New Property', 'wp-real-property'),
-            'edit_item'          => __('Edit Property', 'wp-real-property'),
-            'view_item'          => __('View Property', 'wp-real-property'),
-            'all_items'          => __('All Properties', 'wp-real-property'),
-            'search_items'       => __('Search Properties', 'wp-real-property'),
-            'parent_item_colon'  => __('Parent Properties:', 'wp-real-property'),
-            'not_found'          => __('No properties found.', 'wp-real-property'),
-            'not_found_in_trash' => __('No properties found in Trash.', 'wp-real-property')
+        $labels = [
+            'name'                  => _x('Properties', 'post type general name', 'wp-real-property'),
+            'singular_name'         => _x('Property', 'post type singular name', 'wp-real-property'),
+            'menu_name'             => _x('Properties', 'admin menu', 'wp-real-property'),
+            'name_admin_bar'        => _x('Property', 'add new on admin bar', 'wp-real-property'),
+            'add_new'               => _x('Add New', 'property', 'wp-real-property'),
+            'add_new_item'          => __('Add New Property', 'wp-real-property'),
+            'new_item'              => __('New Property', 'wp-real-property'),
+            'edit_item'             => __('Edit Property', 'wp-real-property'),
+            'view_item'             => __('View Property', 'wp-real-property'),
+            'all_items'             => __('All Properties', 'wp-real-property'),
+            'search_items'          => __('Search Properties', 'wp-real-property'),
+            'parent_item_colon'     => __('Parent Properties:', 'wp-real-property'),
+            'not_found'             => __('No properties found.', 'wp-real-property'),
+            'not_found_in_trash'    => __('No properties found in Trash.', 'wp-real-property'),
         ];
 
         $args = [
-            'labels'             => $labels,
-            'public'             => true,
-            'publicly_queryable' => true,
-            'show_ui'            => true,
-            'show_in_menu'       => true,
-            'query_var'          => true,
-            'rewrite'            => ['slug' => 'real-property'],
-            'capability_type'    => 'post',
-            'has_archive'        => true,
-            'hierarchical'       => false,
-            'menu_position'      => null,
-            'supports'           => ['title', 'editor', 'thumbnail', 'custom-fields'],
+            'labels'                => $labels,
+            'public'                => true,
+            'publicly_queryable'    => true,
+            'show_ui'               => true,
+            'show_in_menu'          => true,
+            'query_var'             => true,
+            'rewrite'               => [ 'slug' => 'real-property' ],
+            'capability_type'       => 'post',
+            'has_archive'           => true,
+            'hierarchical'          => false,
+            'menu_position'         => null,
+            'supports'              => [ 'title', 'editor', 'thumbnail', 'custom-fields' ],
         ];
 
         register_post_type('real-property', $args);
-
     }
-
-
-
-
-
 }
+
+// Initialize the WP_Real_Property class
+//WP_Real_Property::getInstance();
